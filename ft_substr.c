8bd35d86d770f94aaf6a288bcae07993c2ddac2d
Titouan Seche <tseche@3G5.42angouleme.fr>
2025-10-15 16:48:40 +0200

    norm

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tseche <tseche@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 17:38:36 by tseche            #+#    #+#             */
-/*   Updated: 2025/10/15 16:46:49 by tseche           ###   ########.fr       */
+/*   Updated: 2025/10/15 16:48:18 by tseche           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ptr;
 	size_t	tmp;
-	
+
 	ptr = (char *)malloc(sizeof(char) * (len - start));
 	if (!ptr)
 		return (NULL);
 	tmp = ft_strlcpy(ptr, s, len - start);
 	return (tmp);
-} 
+}
